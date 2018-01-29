e44f4c3fb5fdf65669507be8de5e1b0bfce67fe5
LA The Fly Rebel <flyrebel@server.theflyrebel.com>
2018-01-28 21:21:07 -0500

    added updates to contact form

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -132,46 +132,72 @@
 		 * Contact form ajax
 		/* ---------------------------------------------- */
 
-		$('#contact-form').submit(function(e) {
+		$("#contact-form input,#contact-form textarea").jqBootstrapValidation({
+		    preventSubmit: true,
+		    submitError: function($form, event, errors) {
+		      // additional error messages or events
+		    },
+		    submitSuccess: function($form, event) {
+		      event.preventDefault(); // prevent default submit behaviour
+		      // get values from FORM
+		      var name = $("input#c_name").val();
+		      var email = $("input#c_email").val();
+		      var message = $("textarea#c_message").val();
+		      var firstName = name; // For Success/Failure Message
+		      // Check for white space in name for Success/Fail message
+		      if (firstName.indexOf(' ') >= 0) {
+		        firstName = name.split(' ').slice(0, -1).join(' ');
+		      }
+		      $this = $("#sendMessageButton");
+		      $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
+		      $.ajax({
+		        url: "./php/contact.php",
+		        type: "POST",
+		        data: {
+		          name: name,
+		          email: email,
+		          message: message
+		        },
+		        cache: false,
+		        success: function() {
+		        	console.log(data)
+		          // Success message
+		          $('#success').html("<div class='alert alert-success'>");
+		          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+		            .append("</button>");
+		          $('#success > .alert-success')
+		            .append("<strong>Your message has been sent. </strong>");
+		          $('#success > .alert-success')
+		            .append('</div>');
+		          //clear all fields
+		          $('#contactForm').trigger("reset");
+		        },
+		        error: function() {
+		          // Fail message
+		          $('#success').html("<div class='alert alert-danger'>");
+		          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+		            .append("</button>");
+		          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+		          $('#success > .alert-danger').append('</div>');
+		          //clear all fields
+		          $('#contactForm').trigger("reset");
+		        },
+		        complete: function() {
+		          setTimeout(function() {
+		            $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+		          }, 1000);
+		        }
+		      });
+		    },
+		    filter: function() {
+		      return $(this).is(":visible");
+		    },
+		  });
 
-			e.preventDefault();
-
-			var c_name = $('#c_name').val();
-			var c_email = $('#c_email').val();
-			var c_message = $('#c_message ').val();
-			var response = $('#contact-form .ajax-response');
-			
-			var formData = {
-				'name'       : c_name,
-				'email'      : c_email,
-				'message'    : c_message
-			};
-
-			if (( c_name== '' || c_email == '' || c_message == '') || (!isValidEmailAddress(c_email) )) {
-				response.fadeIn(500);
-				response.html('<i class="fa fa-warning"></i> Please fix the errors and try again.');
-			}
-
-			else {
-							console.log(formData)
-
-					 $.ajax({
-							type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
-							url         : 'https://cors-anywhere.herokuapp.com/assets/php/contact.php', // the url where we want to POST
-							data        : formData, // our data object
-							dataType    : 'json', // what type of data do we expect back from the server
-							encode      : true,
-							success		: function(res){
-											var ret = $.parseJSON(JSON.stringify(res));
-											response.html(ret.message).fadeIn(500);
-							}
-						}).done(function(res, err){
-							console.log(res)
-							console.log(err)
-						});
-				}           
-            	return false;
-			});
+		  $("a[data-toggle=\"tab\"]").click(function(e) {
+		    e.preventDefault();
+		    $(this).tab("show");
+		  });
 
 	});
 
--- assets/php/contact.php
+++ assets/php/contact.php
@@ -1,33 +1,22 @@
 <?php
-
-if(isset($_POST['message'])){
-
-	$name = $_POST['name'];
-	$email = $_POST['email'];
-	$message = $_POST['message'];
-    
-	
-	$to      = 'leaveil.armstrong@gmail.com';
-	$subject = 'Site Contact Form';
-
-	$headers = 'From: '. $email . "\r\n" .
-    'Reply-To: '. $email . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-
-	$status = mail($to, $subject, $message, $headers);
-
-	if($status == TRUE){	
-		$res['sendstatus'] = 'done';
-	
-		//Edit your message here
-		$res['message'] = 'Form Submission Successful';
-    }
-	else{
-		$res['message'] = 'Failed to send mail. Please email me at leaveil.armstrong@gmail.com';
-	}
-	
-	
-	echo json_encode($res);
-}
-
+	// Check for empty fields
+	if(empty($_POST['c_name'])  		||
+	   empty($_POST['c_email']) 		||
+	   empty($_POST['c_message'])	||
+	   !filter_var($_POST['c_email'],FILTER_VALIDATE_EMAIL))
+	   {
+		echo "No arguments Provided!";
+		return false;
+	   }
+	$name = strip_tags(htmlspecialchars($_POST['c_name']));
+	$email_address = strip_tags(htmlspecialchars($_POST['c_email']));
+	$message = strip_tags(htmlspecialchars($_POST['c_message']));
+	// Create the email and send the message
+	$to = 'leaveil.armstrong@gmail.com';
+	$email_subject = "Website Contact Form:  $name";
+	$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+	$headers = "From: noreply@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+	$headers .= "Reply-To: $email_address";
+	mail($to,$email_subject,$email_body,$headers);
+	return true;
 ?>
